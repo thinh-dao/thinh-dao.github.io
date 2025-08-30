d6114c77108ac4ac0dd9767ca3e973967d19397a
thinh-dao <ducthinh19102003@gmail.com>
2025-08-30 23:45:26 +0800

    Fix PostLayout.astro syntax error and update astro.config.mjs for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thinh-dao.github.io',
+  base: '/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
